d915f52d253af40f5bef4aac0c0842ebbdd31963
Pedro Guzman <my.rg.developer@gmail.com>
2021-04-21 00:00:44 -0500

    Se cambio el placeholder del input buscar de la barra de navegacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,7 +125,7 @@
                 <!-- Formualario busqueda de modulos -->
                 <div class="form-inline">
                     <div class="input-group" data-widget="sidebar-search">
-                        <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                        <input class="form-control form-control-sidebar" type="search" placeholder="Buscar ..."
                             aria-label="Search">
                         <div class="input-group-append">
                             <button class="btn btn-sidebar">
